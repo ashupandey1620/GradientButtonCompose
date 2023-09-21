b995583fe7fc8c140b3369d0a860b9b9976eaa00
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-09-21 09:42:39 +0530

    Initial commit

---
+++ app/src/main/java/com/compose/gradientbutton/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.compose.gradientbutton
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.compose.gradientbutton.ui.theme.GradientButtonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GradientButtonTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize() ,
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val cornerRadius = 16.dp
+                    val gradientColor = listOf(Color(0xFFff00cc), Color(0xFF333399))
+                    GradientButton(
+                        gradientColors = gradientColor,
+                        cornerRadius = cornerRadius,
+                        nameButton = "Style: top Start",
+                        roundedCornerShape = RoundedCornerShape(topStart = 30.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String , modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!" ,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GradientButtonTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+private fun GradientButton(
+    gradientColors: List<Color> ,
+    cornerRadius: Dp ,
+    nameButton: String ,
+    roundedCornerShape: RoundedCornerShape
+) {
+
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 32.dp, end = 32.dp),
+        onClick = {
+            //your code
+        },
+
+        contentPadding = PaddingValues() ,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent
+        ),
+        shape = RoundedCornerShape(cornerRadius)
+    ) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    brush = Brush.horizontalGradient(colors = gradientColors),
+                    shape = roundedCornerShape
+                )
+                .clip(roundedCornerShape)
+                /*.background(
+                    brush = Brush.linearGradient(colors = gradientColors),
+                    shape = RoundedCornerShape(cornerRadius)
+                )*/
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = nameButton,
+                fontSize = 20.sp,
+                color = Color.White
+            )
+        }
+    }
+}
